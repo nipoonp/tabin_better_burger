44fd3ce533cd0ce6582fc74afce3b745b20b33c2
Nipoon Patel <patel_nipoon@yahoo.co.nz>
2024-09-27 14:54:46 +1200

    Update app icon and name

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Brew\'d Hawt',
+      title: 'Better Burger',
       theme: ThemeData(
         // This is the theme of your application.
         //
