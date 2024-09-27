4eff899dc06143148a23b32a040154613e4ab5c5
Nipoon Patel <patel_nipoon@yahoo.co.nz>
2024-09-27 13:27:42 +1200

    Add

---
+++ android/app/src/main/kotlin/com/tabin/betterburger/betterburger/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.tabin.betterburger.betterburger
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/homepage.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class Homepage extends StatefulWidget {
+  @override
+  HomepageState createState() => HomepageState();
+}
+
+class HomepageState extends State<Homepage> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize WebView controller
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse('https://betterburger.tabin.co.nz/'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFFFFFF),
+      body: SafeArea(
+        child: WillPopScope(
+          onWillPop: () => _goBack(context),
+          child: WebViewWidget(controller: _controller),
+        ),
+      ),
+    );
+  }
+
+  Future<bool> _goBack(BuildContext context) async {
+    if (await _controller.canGoBack()) {
+      _controller.goBack();
+      return Future.value(false);
+    } else {
+      return Future.value(true);
+    }
+  }
+}
